b93fdf57d0cddc7ca2edf2d3f056146634a9a9e0
Deniss Garanin <denis.garanin@alvadi.ee>
2015-10-02 17:29:14 +0300

    ready

--- js.js
+++ js.js
@@ -1,46 +1,43 @@
-window.onload = function()
-{
+window.onload = function () {
     var canvas = document.getElementById('field');
     var game = new Game(canvas);
     var btn = document.getElementById('nextgen');
-    btn.onclick = function(){
+    btn.onclick = function () {
         game.nextGen();
     };
     game.start();
 };
 
 function Point(x, y) {
-    return {x:x, y:y};
+    return {x: x, y: y};
 }
 
-function Game(canvas)
-{
+function Game(canvas) {
     var cellSize = 10;
-    var gridSizeX = 5;
-    var gridSizeY = 5;
+    var gridSizeX = 7;
+    var gridSizeY = 7;
     var grid = [];
 
     canvas.width = gridSizeX * cellSize;
     canvas.height = gridSizeY * cellSize;
     var context = canvas.getContext('2d');
 
-    this.start = function()
-    {
+    this.start = function () {
         generateGrid();
         drawGrid();
-        nextGeneration();
-        /*setInterval(nextGeneration, 300);*/
+
+        setInterval(nextGeneration, 100);
     };
 
-    this.nextGen = function(){
+    this.nextGen = function () {
         nextGeneration();
     };
 
     function nextGeneration() {
         var nextGeneration = [];
-        for(var i = 0; i < grid.length; i++) {
+        for (var i = 0; i < grid.length; i++) {
             nextGeneration[i] = [];
-            for(var j = 0; j < grid[i].length; j++) {
+            for (var j = 0; j < grid[i].length; j++) {
                 nextGeneration[i][j] = isAlive(new Point(j, i));
             }
         }
@@ -51,37 +48,44 @@ function Game(canvas)
     function isAlive(point) {
         var neighbors = getNeighbors(point);
         var liveNeighborsNumber = 0;
-        for(var i in neighbors) {
+        for (var i in neighbors) {
             var p = neighbors[i];
-            if(grid[p.y][p.x]) {
+            if (grid[p.y][p.x]) {
                 liveNeighborsNumber++;
             }
         }
-        console.log(liveNeighborsNumber);
-        if(grid[p.y][p.x] == false && liveNeighborsNumber == 3) {
-            return true;
-        }
-        if(grid[p.y][p.x] == true) {
-            return liveNeighborsNumber == 2 || liveNeighborsNumber == 3;
+
+        if(grid[point.y][point.x]) {
+            return !!(liveNeighborsNumber == 2 || liveNeighborsNumber == 3);
+        } else {
+            return liveNeighborsNumber == 3;
         }
     }
 
-    function generateGrid()
-    {
-        for(var i = 0; i < gridSizeY; i++) {
-            grid[i] = [];
-            for(var j = 0; j < gridSizeX; j++) {
-                grid[i][j] = Math.random() >= 0.5;
-            }
-        }
+    function generateGrid() {
+        /*        for(var i = 0; i < gridSizeY; i++) {
+         grid[i] = [];
+         for(var j = 0; j < gridSizeX; j++) {
+         grid[i][j] = Math.random() >= 0.5;
+         }
+         }*/
+        grid = [
+            [false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false],
+            [false, false, false, true, false, false, false],
+            [false, false, false, false, true, false, false],
+            [false, false, true, true, true, false, false],
+            [false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false]
+        ];
     }
 
-    function drawGrid()
-    {
+    function drawGrid() {
         context.clearRect(0, 0, canvas.width, canvas.height);
-        for(var i = 0; i < grid.length; i++) {
-            for(var j = 0; j < grid[i].length; j++) {
-                if(grid[i][j]) {
+        for (var i = 0; i < grid.length; i++) {
+            for (var j = 0; j < grid[i].length; j++) {
+                if (grid[i][j]) {
                     context.fillRect(j * cellSize, i * cellSize, cellSize, cellSize);
                 }
             }
@@ -89,29 +93,29 @@ function Game(canvas)
     }
 
     function getNeighbors(point) {
-        var neighbors =  [
+        var neighbors = [
             new Point(point.x - 1, point.y - 1),
-            new Point(point.x,     point.y - 1),
+            new Point(point.x, point.y - 1),
             new Point(point.x + 1, point.y - 1),
             new Point(point.x - 1, point.y),
             new Point(point.x + 1, point.y),
             new Point(point.x - 1, point.y + 1),
-            new Point(point.x,     point.y + 1),
+            new Point(point.x, point.y + 1),
             new Point(point.x + 1, point.y + 1)
         ];
 
-        for(var i in neighbors) {
+        for (var i in neighbors) {
             var p = neighbors[i];
-            if(p.y >= gridSizeY) {
+            if (p.y >= gridSizeY) {
                 neighbors[i].y = 0;
             }
-            if(p.y < 0) {
+            if (p.y < 0) {
                 neighbors[i].y = gridSizeY - 1;
             }
-            if(p.x >= gridSizeX) {
+            if (p.x >= gridSizeX) {
                 neighbors[i].x = 0;
             }
-            if(p.x < 0 ) {
+            if (p.x < 0) {
                 neighbors[i].x = gridSizeX - 1;
             }
         }
